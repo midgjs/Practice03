15efeaaee93c0028885efdf1af067896c79b5fb9
midgjs <sjqksk.10@gmail.com>
2022-04-20 23:28:09 +0900

    2022-04-20

---
+++ src/com/javaex/practice/Ex14.java
@@ -0,0 +1,36 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex14 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요: ");
+		int num = sc.nextInt();
+		int sum=0;
+		
+		for(int i=1; i<=num; i++) {
+			if(i==num) {
+				sum = sum+i;
+				System.out.println(i);
+				System.out.println("합계: " + sum);
+			} else { 
+				sum = sum+i;
+				System.out.print(i + "+");
+			}
+		}
+		
+		
+		
+		sc.close();
+		
+		
+		
+		
+		
+	}
+
+}
