a0fadd5126e2233b4d963c71ad2d7df40d239e32
midgjs <sjqksk.10@gmail.com>
2022-04-22 18:16:32 +0900

    2

---
+++ src/com/javaex/practice/Ex17.java
@@ -0,0 +1,33 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex17 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요: ");
+		int num = sc.nextInt();
+		System.out.println("5의 배수의 개수: " + num/5);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+		
+		
+		
+		
+		
+	}
+
+}
