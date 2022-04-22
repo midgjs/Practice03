3ddde21385468f963754142bc9ace1afbdfb86df
midgjs <sjqksk.10@gmail.com>
2022-04-22 15:47:24 +0900

    2022-04-22

---
+++ src/com/javaex/practice/Ex12.java
@@ -0,0 +1,30 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요");
+		int num = sc.nextInt();
+		int fac = 1;
+		for(int i=1; i<=num; i++) {
+			fac = fac*i;
+		
+			
+		}
+		System.out.println(fac);
+		
+		
+		sc.close();
+		
+		
+		
+		
+		
+		
+	}
+
+}
